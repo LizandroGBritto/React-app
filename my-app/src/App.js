d10be2b1b7cc2c4674b7ae39be4cdc110f194cf3
LizandroGBritto <80988976+LizandroGBritto@users.noreply.github.com>
2023-07-14 21:16:59 -0400

    "Add the Header, footer and Navigation Menu

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import HelloWorld from './Components/HelloWorld'
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+   <div>
+    <Header/>
+
+    <HelloWorld name=" Lizandro"/>
+
+    <Footer/>
+
+   </div>
+  );
+}
+
+export default App;
